d2805c2ed9bd6b49d98dbf66faea56ce06c47322
Mümin Yıldırım <mmnyldrm61@gmail.com>
2020-05-22 11:59:17 +0300

    Update words.js

--- pages/api/words.js
+++ pages/api/words.js
@@ -1,31 +1,12 @@
 
 
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-
-        // Pick a remaining element...
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-
-        // And swap it with the current element.
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-}
-
-export default (request, response) => {
+export default function words(request, response) {
     // request status code 
     // all request method allowed
     
     const { API_KEY } = request.body;
     if ( request.method === 'POST' && API_KEY === process.env.API_KEY ) {
-        response.status(200).json(shuffle([1,2,3,4,5]));
+        response.status(200).json({s: 1});
     } else {
         response.status(200).json({
             code: 10010, 
