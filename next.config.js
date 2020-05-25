17234a5c8b2ce98ee84d5c3ae79066ad4410f995
Mümin Yıldırım <mmnyldrm61@gmail.com>
2020-05-25 20:52:14 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
 module.exports = {
   env: {
     API_KEY: '29c40030-b009-4e1e-a737-69d10bd2e4ca',
-  },
-  webpack: config => {
-    config.module.rules.push({
-      test: /\.md$/,
-      use: "raw-loader"
-    });
-
-    return config;
   }
 }
