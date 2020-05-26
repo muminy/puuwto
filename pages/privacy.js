5571aa835fa4b44741cbe83e386756e72e367f5e
Mümin Yıldırım <mmnyldrm61@gmail.com>
2020-05-26 17:53:18 +0300

    privacy

--- pages/privacy.js
+++ pages/privacy.js
@@ -9,69 +9,31 @@ export default function privacy(){
             </Head>
             <div className="privacy">
             <h1 className="firstHeader">Privacy Policy</h1>
-            <p>
-            puuwto (“we” or “us” or “our”) respects the privacy of our users (“user” or “you”). This Privacy Policy explains how we collect, use, disclose, and safeguard your information when you visit our mobile application (the “Application”). Please read this Privacy Policy carefully. IF YOU DO NOT AGREE WITH THE TERMS OF THIS PRIVACY POLICY, PLEASE DO NOT ACCESS THE APPLICATION.
-            </p>
-
-            <p>
-            We reserve the right to make changes to this Privacy Policy at any time and for any reason. We will alert you about any changes by updating the “Last updated” date of this Privacy Policy. You are encouraged to periodically review this Privacy Policy to stay informed of updates. You will be deemed to have been made aware of, will be subject to, and will be deemed to have accepted the changes in any revised Privacy Policy by your continued use of the Application after the date such revised Privacy Policy is posted.
-            </p>
-
-            <p>
-            This Privacy Policy does not apply to the third-party online/mobile store from which you install the Application or make payments, including any in-game virtual items, which may also collect and use data about you. We are not responsible for any of the data collected by any such third party.
-            </p>
-
-            <p>
-            This Privacy Policy was created using Termly’s Privacy Policy Generator.
-            </p>
-
-            <h1>
-            COLLECTION OF YOUR INFORMATION
-            </h1>
-
-            <p>
-            We may collect information about you in a variety of ways. The information we may collect via the Application depends on the content and materials you use, and includes:
-            </p>
-
-            <h1>
-            Personal Data
-            </h1>
-
-            <p>
-            Demographic and other personally identifiable information (such as your name and email address) that you voluntarily give to us when choosing to participate in various activities related to the Application, such as chat, posting messages in comment sections or in our forums, liking posts, sending feedback, and responding to surveys. If you choose to share data about yourself via your profile, online chat, or other interactive areas of the Application, please be advised that all data you disclose in these areas is public and your data will be accessible to anyone who accesses the Application.
-            </p>
-
-            <h1>
-            Derivative Data
-            </h1>
-
-            <p>
-            Information our servers automatically collect when you access the Application, such as your native actions that are integral to the Application, including liking, re-blogging, or replying to a post, as well as other interactions with the Application and other users via server log files.
-            </p>
-            <h1>
-            Push Notifications
-            </h1>
-            <p>
-            We may request to send you push notifications regarding your account or the Application. If you wish to opt-out from receiving these types of communications, you may turn them off in your device’s settings.
-            </p>
-            <h1>
-            Third-Party Data
-            </h1>
-            <p>
-            Information from third parties, such as personal information or network friends, if you connect your account to the third party and grant the Application permission to access this information.
-            </p>
-            <h1>
-            Data From Contests, Giveaways, and Surveys
-            </h1>
-            <p>
-            Personal and other information you may provide when entering contests or giveaways and/or responding to surveys.
-            </p>
-            <h1>
-            CONTACT US
-            </h1>
-            <p>
-            If you have questions or comments about this Privacy Policy, please contact us at:
-            </p>
+            <p> puuwto (“we” or “us” or “our”) respects the privacy of our users (“user” or “you”). This Privacy Policy explains how we collect, use, disclose, and safeguard your information when you visit our mobile application (the “Application”). Please read this Privacy Policy carefully. IF YOU DO NOT AGREE WITH THE TERMS OF THIS PRIVACY POLICY, PLEASE DO NOT ACCESS THE APPLICATION. </p>
+            <p>  We reserve the right to make changes to this Privacy Policy at any time and for any reason. We will alert you about any changes by updating the “Last updated” date of this Privacy Policy. You are encouraged to periodically review this Privacy Policy to stay informed of updates. You will be deemed to have been made aware of, will be subject to, and will be deemed to have accepted the changes in any revised Privacy Policy by your continued use of the Application after the date such revised Privacy Policy is posted. </p>
+            <p> This Privacy Policy does not apply to the third-party online/mobile store from which you install the Application or make payments, including any in-game virtual items, which may also collect and use data about you. We are not responsible for any of the data collected by any such third party. </p>
+            <p> This Privacy Policy was created using Termly’s Privacy Policy Generator. </p>
+            <h1>What personal information do we collect from the people that visit our blog, website or app?</h1>
+            <p>When ordering or registering on our site, as appropriate, you may be asked to enter your email address or other details to help you with your experience.</p>
+            <h1> When do we collect information? </h1>
+            <p> We collect information from you when you subscribe to a newsletter or enter information on our site. </p>
+            <h1> Personal Data </h1>
+            <p> Demographic and other personally identifiable information (such as your name and email address) that you voluntarily give to us when choosing to participate in various activities related to the Application, such as chat, posting messages in comment sections or in our forums, liking posts, sending feedback, and responding to surveys. If you choose to share data about yourself via your profile, online chat, or other interactive areas of the Application, please be advised that all data you disclose in these areas is public and your data will be accessible to anyone who accesses the Application. </p>
+            <h1> Derivative Data </h1>
+            <p> Information our servers automatically collect when you access the Application, such as your native actions that are integral to the Application, including liking, re-blogging, or replying to a post, as well as other interactions with the Application and other users via server log files. </p>
+            <h1> Push Notifications </h1>
+            <p> We may request to send you push notifications regarding your account or the Application. If you wish to opt-out from receiving these types of communications, you may turn them off in your device’s settings. </p>
+            <h1>Do we use 'cookies'?</h1>
+            <p>We do not use cookies for tracking purposes.</p>
+            <p>You can choose to have your computer warn you each time a cookie is being sent, or you can choose to turn off all cookies. You do this through your browser settings. Since browser is a little different, look at your browser's Help Menu to learn the correct way to modify your cookies.</p>
+            <p>If you turn cookies off, some features will be disabled. that make your site experience more efficient and may not function properly.</p>
+            <p>However, you will still be able to place orders .</p>
+            <h1> Third-Party Data </h1>
+            <p> Information from third parties, such as personal information or network friends, if you connect your account to the third party and grant the Application permission to access this information. </p>
+            <h1> Data From Contests, Giveaways, and Surveys </h1>
+            <p> Personal and other information you may provide when entering contests or giveaways and/or responding to surveys. </p>
+            <h1> CONTACT US </h1>
+            <p> If you have questions or comments about this Privacy Policy, please contact us at: </p>
             <p className="sirket">puuwto</p>
             <div className="mail">mmnyldrm61@gmail.com</div>
             </div>
