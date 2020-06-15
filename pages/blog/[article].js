d3838b470c3ca872df74420da0e2d8b4b9c7f80c
Mümin Yıldırım <mmnyldrm61@gmail.com>
2020-06-15 22:42:46 +0300

    0.2

--- pages/blog/[article].js
+++ pages/blog/[article].js
@@ -4,35 +4,17 @@ import { Api } from '../../constant/Api';
 import ArticleContent from '../../components/Article';
 import Footer from "../../components/Footer";
 import Head from 'next/head';
-import { useRouter } from 'next/router';
-import { useState, useEffect } from "react";
-import Loading from "../../components/Loading";
-const Article = () => {
 
-  const route = useRouter();
-  const [content, setContent] = useState(null);
-  
-  const getContent = () => {
-    fetch(Api + '/blog/' + route.query.article)
-    .then(responseJson => responseJson.json())
-    .then(responseJson => setContent(responseJson.content))
-  }
-
-  useEffect(() => {
-    getContent()
-  }, [])
-
+const Article = ({content}) => {
   return (
     <div>
       <Head>
         <title> | puuwto.com</title>
       </Head>
       <Header />
-      {content ? (
-        <ArticleContent 
-          content={content.content}
-          title={content.title} />
-      ) : <Loading />}
+      <ArticleContent 
+        content={content.content}
+        title={content.title} />
       <Footer />
     </div>
   )
