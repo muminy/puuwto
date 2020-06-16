8a33c4a1ab1a6d275a834d328a0d5434474ad150
Mümin Yıldırım <mmnyldrm61@gmail.com>
2020-06-16 15:07:09 +0300

    0.3

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,13 @@
 import Footer from "./Footer";
 import Header from "./Header";
-import Head from 'next/head'
+import Head from "next/head";
 export default function ({ title, children }) {
   return (
     <div className="layout">
       <Head>
-        <title>{title + ' |'} puuwto.com</title>
+        <title>
+          {title} {title ? " |" : null} puuwto.com {!title ? '| Kişisel blog' : null}
+        </title>
         <meta
           name="viewport"
           content="initial-scale=1.0, width=device-width"
