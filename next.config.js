99517ce26b72c0abf28005de6c5c5e8cecbaafd1
Mümin Yıldırım <mmnyldrm61@gmail.com>
2020-08-02 20:24:45 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const isDev = process.env.NODE_ENV === "development";
 
 module.exports = {
   env: {
-    PER_PAGE_BLOG: isDev ? 1 : 5,
+    PER_PAGE_BLOG: isDev ? 1 : 1,
   },
   webpack: (config) => {
     config.module.rules.push({
