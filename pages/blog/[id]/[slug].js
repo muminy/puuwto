bffff2031d3c3066232591421137de7884ee6e7f
Mümin Yıldırım <mmnyldrm61@gmail.com>
2020-08-21 15:53:28 +0300

    try to fix pages

--- pages/blog/[id]/[slug].js
+++ pages/blog/[id]/[slug].js
@@ -1,26 +1,38 @@
-import fs from "fs";
-import path from "path";
-import matter from "gray-matter";
 import Layout from "components/Layout";
 import ArticleComponent from "components/Article";
-import { useContext, useState } from "react";
-import LanguageContext from "context/LanguageContext";
-export default function BlogContent({ slug, id }) {
-  const { posts } = useContext(LanguageContext);
-  const [article, setArticle] = useState(
-    posts.filter(
-      (item) =>
-        item.slug === slug && item.id === parseInt(id),
-    )[0],
-  );
+import { useContext, useState, useEffect } from "react";
+import getPosts from "lib/getPosts";
+
+export default function BlogContent(props) {
+  console.log(props);
   return (
     <Layout>
-      <ArticleComponent {...article} />
+      <ArticleComponent {...props} />
     </Layout>
   );
 }
 
-BlogContent.getInitialProps = ({ query }) => {
-  const { id, slug } = query;
-  return { slug, id };
+export const getStaticProps = ({
+  params: { slug, id },
+}) => {
+  const posts = getPosts();
+  const postIndex = posts.findIndex(
+    (p) => p.slug === slug && p.id === parseInt(id),
+  );
+  const post = posts[postIndex];
+
+  return {
+    props: {
+      previous: posts[postIndex - 1] || null,
+      next: posts[postIndex + 1] || null,
+      ...post,
+    },
+  };
+};
+
+export const getStaticPaths = () => {
+  return {
+    paths: getPosts().map((p) => `/blog/${p.id}/${p.slug}`),
+    fallback: false,
+  };
 };
